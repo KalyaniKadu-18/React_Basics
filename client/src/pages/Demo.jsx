4b3f83831f61ce502ae817231f0ee8887ce07365
Kalyani Kadu <kadukalyani18@gmail.com>
2026-07-30 22:43:05 +0530

    "use memo code"

--- client/src/pages/Demo.jsx
+++ client/src/pages/Demo.jsx
@@ -15,28 +15,55 @@
 
 // export default Demo;
 
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+
+// function Demo() {
+
+//     const [count,setCount] = useState(0);
+
+// //     useEffect (() => {
+// //     }); //Runs after every render
+
+// // useEffect (() => {
+// // },[]); //runs after first render
+
+// useEffect (() => { 
+//     alert("count has been changed")
+// },[count]); // runs when count changes
+
+// // useEffect (() => {},[count,name]); //runs when count and name changes
+
+//   return (
+//     <div>
+//        <h2>count:{count}</h2>
+//        <button onClick={() => setCount(count+1)}>Increase</button>
+//     </div>
+//   )
+// }
+
+// export default Demo
+
+import React, { useMemo, useState } from 'react'
 
 function Demo() {
 
-    const [count,setCount] = useState(0);
+const[num,setNum] = useState(2);
+const[val,setVal] = useState(0);
 
-//     useEffect (() => {
-//     }); //Runs after every render
-
-// useEffect (() => {
-// },[]); //runs after first render
-
-useEffect (() => { 
-    alert("count has been changed")
-},[count]); // runs when count changes
-
-// useEffect (() => {},[count,name]); //runs when count and name changes
+const square = useMemo(() => {
+   return num*num;
+},[num])
 
   return (
     <div>
-       <h2>count:{count}</h2>
-       <button onClick={() => setCount(count+1)}>Increase</button>
+        <h1>num : {num}</h1>
+        <h2>square: {square}</h2>
+        <button onClick={() => setNum(num+1)}>
+            increase
+        </button>
+        <button onClick={() => setVal(val + 1)}>
+            count:{val}
+        </button>
     </div>
   )
 }
